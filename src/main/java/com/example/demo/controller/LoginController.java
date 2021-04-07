a8f0e421797260c585a0fd114dc5079f5e988949
Administrator <admin@qq.com>
2021-04-07 22:40:30 +0800

    修改了登录注册功能

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -11,85 +11,100 @@ import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Api(tags = "登录注册控制类")
-@Controller
+@RestController
 public class LoginController {
 
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/toLogin")
-    public String toLogin() {
-        return "redirect:";
-    }
-
-    @PostMapping(value = "/Login")
-    public String login(String username, String password, Model model) {
-        Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, password);
-        try {
-            subject.login(usernamePasswordToken);
-            return "view/index";
-        } catch (UnknownAccountException e) {
-            model.addAttribute("msg", "用户名不存在");
-            return "view/login";
-        } catch (IncorrectCredentialsException e) {
-            model.addAttribute("msg", "密码错误");
-            return "view/login";
-        }
-    }
-
     @GetMapping("/index")
     public String index() {
         return "view/index";
     }
 
-    @GetMapping("/index/add")
-    public String add() {
-        return "view/add";
+    @GetMapping("/toLogin")
+    public String toLogin() {
+        return "login";
     }
 
-    @GetMapping("/index/update")
-    public String update() {
-        return "view/update";
+    @PostMapping(value = "/Login")
+    public Map<Object, Object> login(String username, String password) {
+        Subject subject = SecurityUtils.getSubject();
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        if (username != null & password != null) {
+            hashMap.put("username", username);
+            UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, password);
+            try {
+                subject.login(usernamePasswordToken);
+                hashMap.put("msg", "登录成功");
+                hashMap.put("status", "OK");
+                return hashMap;
+            } catch (UnknownAccountException e) {
+                hashMap.put("msg", "用户名不存在");
+                hashMap.put("status", "Error");
+                return hashMap;
+            } catch (IncorrectCredentialsException e) {
+                hashMap.put("msg", "密码错误");
+                hashMap.put("status", "Error");
+                return hashMap;
+            }
+        }
+        hashMap.put("msg", "用户名及密码不能为空");
+        hashMap.put("status", "Error");
+        return hashMap;
     }
+
 
     @GetMapping("/logOut")
-    public String logOut(Model model) {
+    public Map<Object, Object> logOut() {
         Subject subject = SecurityUtils.getSubject();
+        HashMap<Object, Object> hashMap = new HashMap<>();
         if (subject.isAuthenticated()) {
             subject.logout();
-            model.addAttribute("msg", "已登出");
-            return "view/index";
+            hashMap.put("status", "ok");
+            hashMap.put("msg", "已登出");
+            return hashMap;
         } else {
-            model.addAttribute("msg", "登出失败,请先登录");
-            return "view/login";
+            hashMap.put("status", "error");
+            hashMap.put("msg", "登出失败,请先登录");
+            return hashMap;
         }
     }
 
     @GetMapping("/toSignIn")
     public String toSignIn() {
-        return "view/signin";
+        return "signin";
     }
 
     @PostMapping("/signIn")
-    public String signIn(String username, String password, String phone, String email, Model model) {
-        User user = userService.getUserByName(username);
-        if (user != null) {
-            model.addAttribute("msg", "用户已存在");
-            return "view/signin";
-        } else {
-            String newPassword = new Md5Hash(password, ByteSource.Util.bytes(username)).toHex();
-            userService.addUser(new User(0, username, newPassword, phone, email));
-            return "view/index";
+    public Map<Object, Object> signIn(String username, String password, String phone, String email) {
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        if (username != null & password != null & phone != null & email != null) {
+            User user = userService.getUserByName(username);
+            if (user != null) {
+                hashMap.put("status", "error");
+                hashMap.put("msg", "用户已存在");
+                return hashMap;
+            } else {
+                String newPassword = new Md5Hash(password, ByteSource.Util.bytes(username)).toHex();
+                userService.addUser(new User(0, username, newPassword, phone, email));
+                hashMap.put("status", "OK");
+                hashMap.put("msg", "注册成功");
+                return hashMap;
+            }
         }
+        hashMap.put("status", "Error");
+        hashMap.put("msg", "不能留空, 请填写完所有内容");
+        return hashMap;
     }
 }
